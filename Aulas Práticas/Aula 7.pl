464b50b7b7be0bb88cb8bb91b7b65e3f9950bdb4
Enzo-Nunes <pedralhoenzo@gmail.com>
2022-12-19 11:24:30 +0000

    aula 7

---
+++ Aulas Práticas/Aula 7.pl
@@ -0,0 +1,18 @@
+insere_ordenado(E, L1, L2) :-
+    findall(M, (member(M, L1), M < E)),
+    append(Menores, Maiores, L1),
+    append(Menores, [E, Temp]),
+    append(Temp, Maiores, L2).
+
+junta_novo_aleatorio(L1, Lim_Inf, Lim_Sup, L2) :-
+    findall(M, between(Lim_Inf, Lim_Sup, M), Todos),
+    subtract(Todos, L1, Res),
+    length(Res, Length),
+    random_between(1, Length, Index),
+    nth1(Index, Res, N),
+    insere_ordenado(N, L1, L2).
+    
+
+repete_el(E, N, L) :-
+    length(L1, N),
+    findall(E, member(N, L1), L).
